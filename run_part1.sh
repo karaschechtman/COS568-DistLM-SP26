4679ef570bc6dbb88d867b124c92c79466dafa67
Kara Schechtman <schechtk@princeton.edu>
2026-03-19 18:58:09 -0400

    Clean up run_part1.sh by removing unnecessary commands
    Removed package installation commands and bashrc update.

--- run_part1.sh
+++ run_part1.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install htop dstat python3-pip
-echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
-source ~/.bashrc
-
 pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 --index-url https://download.pytorch.org/whl/cpu
 pip install numpy scipy scikit-learn tqdm pytorch_transformers apex
 
