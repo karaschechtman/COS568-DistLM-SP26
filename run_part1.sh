e53ca522810edb170f81d1bededb50b99ec2f796
karaschechtman <schechtk@princeton.edu>
2026-03-20 11:38:49 -0400

    remove unecesssary cd

--- run_part1.sh
+++ run_part1.sh
@@ -3,8 +3,6 @@
 pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 --index-url https://download.pytorch.org/whl/cpu
 pip install numpy scipy scikit-learn tqdm pytorch_transformers apex
 
-cd COS568-DistLM-SP26
-
 mkdir -p $HOME/glue_data
 python3 download_glue_data.py --data_dir $HOME/glue_data
 
